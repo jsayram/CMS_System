8cc8875e7ea87569ae713e199124ceb9ba528d09
Jose Ramirez <jsayram@Gmail.com>
2017-08-20 14:25:43 -0400

    Turned on output buffering on top of intialized.php and created error functions, as well as redirect on new.php file

--- globe_bank/private/functions.php
+++ globe_bank/private/functions.php
@@ -28,4 +28,23 @@
         return htmlspecialchars($string);
     }
 
+   //these functions will send error messages
+   function error_404(){
+       header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+       exit();
+
+   }
+
+    function error_500(){
+        header($_SERVER["SERVER_PROTOCOL"] . " 500 Internal Server Error");
+        exit();
+    }
+
+    function redirect_to($location){
+        header("Location: " . $location);
+        exit();
+    }
+
+
+
 ?>
--- globe_bank/private/initialize.php
+++ globe_bank/private/initialize.php
@@ -1,4 +1,6 @@
 <?php
+  //this makes sure output buffering is turned on
+  ob_start();
 
   // Assign file paths to PHP constants
   // __FILE__ returns the current path to this file
