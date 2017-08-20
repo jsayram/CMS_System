85c52d50cdb8b3ddaa84fe64566ca30b4711aad5
Jose Ramirez <jsayram@Gmail.com>
2017-08-19 20:45:26 -0400

    added the encode for the html tags to prevent scaping and the users cant break the application when adding their information, this is found int he show.php file and the special function included in the functions.php file

--- globe_bank/private/functions.php
+++ globe_bank/private/functions.php
@@ -11,4 +11,21 @@
         //NOTE THAT THIS DOES NOT ECHO THE PATH JUST RETURNS IT
         return WWW_ROOT . $script_path;
     }
+
+
+// functions to make my life esier, used to not have to type urlencode or rawurlencode everytime, these make sure we are not using the reserved character in the URL
+    function u($string=""){
+        return urlencode($string);
+    }
+
+    function raw_u($string=""){
+    return rawurlencode($string);
+    }
+
+
+   //this encodes html special characters so that they dont cause problems when placed in a script
+    function h($string=""){
+        return htmlspecialchars($string);
+    }
+
 ?>
--- globe_bank/public/staff/subjects/show.php
+++ globe_bank/public/staff/subjects/show.php
@@ -1,9 +1,28 @@
+
+
+
+<?php //brought in so that we can use the custom functions made that are found in the initilize directory
+require_once('../../../private/initialize.php'); ?>
+
 <?php
 
 //this shows the id based on the items gathered from the subjects/index.php file
-$id = $_GET['id'];
+//tenary operator that checks if the id is set then it returns the id ,if not then set it to '1', is important when you take a value out from the super globals that you check if is there if not set it or else you can get an error whenever the id is not set.
+//code for php less than 7.0
+$id = isset($_GET['id']) ? $_GET['id']: '1' ;
 
-echo $id;
+//same code as above but for php > 7.0
+//$id = $_GET['id'] ?? '1' ;
 
+
+//this show what 'id' was retrieved from the global $_GET variable
+//h is int the custom functions file so that no special characters are rendered
+//this is called escaping so that no special characters get through that could potentially hurt the system
+echo h($id);
 
 ?>
+
+<!--Adds the necessary url id for each of the links that is displayed-->
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo  u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo  u('!#*?'); ?>">Link</a><br />
